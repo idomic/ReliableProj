e11297ed7f80d2d28588baae27424891cbb66c9b
root <root@ip-172-31-86-217.ec2.internal>
2018-04-05 02:54:08 +0000

    added 2 fuzzers bitmap files afl../fuzzer01/bitmaps

--- afl-2.52b/afl_out/shellAutomation.sh
+++ afl-2.52b/afl_out/shellAutomation.sh
@@ -8,23 +8,20 @@ mkdir ./fuzzer02/bitmaps ./fuzzer01/bitmaps
 for file in ./fuzzer01/queue/id*
 do
    # take action on each file.
-   #fname=`basename $file`
+   fname=`basename $file`
    #echo "fileName: $fname"
-   #../afl-showmap -q -o ./fuzzer01/bitmaps/$fname -- /home/ubuntu/binafl/binutils-2.30/binutils/objdump -D $file
-   # cat ./fuzzer01/bitmaps/$fname | cut -d ':' -f1 > ./fuzzer01/bitmaps/$fname
+   ../afl-showmap -q -o ./fuzzer01/bitmaps/$fname -- /home/ubuntu/binafl/binutils-2.30/binutils/objdump -D $file
+    cat ./fuzzer01/bitmaps/$fname | cut -d ':' -f1 > ./fuzzer01/bitmaps/$fname
 done
 
 for file in ./fuzzer02/queue/id*
 do
    # take action on each file.
-   #fname=`basename $file`
+   fname=`basename $file`
    #echo "fileName: $fname"
-   #../afl-showmap -q -o ./fuzzer02/bitmaps/$fname -- /home/ubuntu/binafl/binutils-2.30/binutils/objdump -D $file
-   #cat ./fuzzer02/bitmaps/$fname | cut -d ':' -f1 > ./fuzzer02/bitmaps/$fname
+   ../afl-showmap -q -o ./fuzzer02/bitmaps/$fname -- /home/ubuntu/binafl/binutils-2.30/binutils/objdump -D $file
+   cat ./fuzzer02/bitmaps/$fname | cut -d ':' -f1 > ./fuzzer02/bitmaps/$fname
 done
 date
-#TODO iterate with for loop over bitmaps
-# Remove second coloumn from the bitmap file
-#cat tester | cut -d ':' -f1 > bit
 
 # End of script
