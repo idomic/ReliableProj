7c89b7c63c5a8afeb5239fb441bd2657158f9394
yuvsc <root@instance-2.c.dlforcvhw5.internal>
2018-05-02 18:02:33 +0000

    more adding

--- gen_gcov.py
+++ gen_gcov.py
@@ -1,10 +1,11 @@
 import glob
 import subprocess
-file_list = glob.glob('/home/dongdong/Project/size/adv_gen_seeds/*')
+file_list = glob.glob('/home/reliableProj/ReliableProj/adv_gen_seeds_old/*')
 #seed_list = glob.glob('/home/dongdong/Project/objdump/afl_out/afl_out/queue/*')
-base_list = glob.glob('/home/dongdong/Project/size/afl_out/seeds/*')
+base_list = glob.glob('/home/reliableProj/ReliableProj/afl-2.52b/afl_out/seeds/*')
 import os
 import time
+import sys
 stdout = subprocess.STDOUT
 call = subprocess.call
 FNULL = open(os.devnull, 'w')
@@ -15,18 +16,26 @@ cnt = 0
 idx = 0
 for fll in file_list:
     seed_list = glob.glob(fll+"/*")
+    #print(seed_list)
     for fl in seed_list:
-        if not (idx % 5000):
+	#print(fl)
+        if not (idx % 10):#changed from 5000
             print(100*idx/float(lee))
-        ret = call(["/home/dongdong/Project/size/binutils-2.30_gcov/binutils/size",fl], stdout=FNULL, stderr=stdout)
-        if ret < 0:
-            print("crash " + str(ret) + fl)
-        idx+=1
+        try:
+            ret = call(["/home/reliableProj/ReliableProj/afl_gcov/binutils-2.30/binutils/objdump","-D",fl], stdout=FNULL, stderr=stdout, timeout=3600)
+        
+            if ret < 0:
+                print("crash " + str(ret) + fl)
+            sys.stdout.flush()
+            idx+=1
+        except subprocess.TimeoutExpired: 
+            print(ret) 
 print(time.time()-t0)
-
 '''
 for idx,fl in enumerate(base_list):
     if not (idx % 1000):
         print(100*idx/float(len(base_list)))
-    ret = call(["/home/dongdong/Project/size/binutils-2.30_gcov/binutils/size", fl], stdout=FNULL, stderr=stdout)
+    ret = call(["/home/reliableProj/ReliableProj/afl_gcov/binutils-2.30/binutils/objdump", fl], stdout=FNULL, stderr=stdout)
+
+print(time.time()-t0)
 '''
---
+++ gen_gcov_original.py
@@ -0,0 +1,40 @@
+import glob
+import subprocess
+file_list = glob.glob('/home/reliableProj/ReliableProj/adv_gen_seeds_old/*')
+#seed_list = glob.glob('/home/dongdong/Project/objdump/afl_out/afl_out/queue/*')
+base_list = glob.glob('/home/reliableProj/ReliableProj/afl-2.52b/afl_out/seeds/*')
+import os
+import tensorflow as tf
+import time
+stdout = subprocess.STDOUT
+call = subprocess.call
+FNULL = open(os.devnull, 'w')
+t0 = time.time()
+lee= len(file_list)*51000
+cnt = 0
+
+idx = 0
+'''for fll in file_list:
+    seed_list = glob.glob(fll+"/*")
+    #print(seed_list)
+    for fl in seed_list:
+        if not (idx % 5000):
+            print(100*idx/float(lee))
+        ret = call(["/home/reliableProj/ReliableProj/afl_gcov/binutils-2.30/binutils/objdump",fl], stdout=FNULL, stderr=stdout)
+        if ret < 0:
+            print("crash " + str(ret) + fl)
+        idx+=1
+print(time.time()-t0)
+'''
+#with tf.device('/gpu:0'):
+	#def dothis():
+for idx,fl in enumerate(base_list):
+	if not (idx % 10):
+    		print(100*idx/float(len(base_list)))
+    	ret = call(["/home/reliableProj/ReliableProj/afl_gcov/binutils-2.30/binutils/objdump","-D", fl], stdout=FNULL, stderr=stdout)
+
+print(time.time()-t0)
+
+#with tf.Session() as sess:
+#	print(sess.run(dothis()))
+
